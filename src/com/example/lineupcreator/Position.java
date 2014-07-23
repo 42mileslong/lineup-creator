69a423e68b98109cc5a5b51db3798fb78641fc40
Rafi Long <rafilong42@gmail.com>
2014-07-23 14:54:17 -0700

    PSTN - Created class

---
+++ src/com/example/lineupcreator/Position.java
@@ -0,0 +1,20 @@
+package com.example.lineupcreator;
+
+/**
+ * A single position on the field
+ * 
+ * @author Rafi
+ */
+public class Position {
+	/**
+	 * The player playing the position
+	 */
+	public Player positionPlayer = null;
+	
+	/**
+	 * Default constructor
+	 */
+	public Position() {
+		
+	}
+}
